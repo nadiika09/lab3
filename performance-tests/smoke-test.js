81e6b593edad48c603756d5dfb81aa9e13fe3a22
nadiika09 <nadiika09@ukr.net>
2026-04-10 04:14:16 +0800

    Add smoke test for HTTP status check

--- performance-tests/smoke-test.js
+++ performance-tests/smoke-test.js
@@ -1,1 +1,8 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
 
+export default function () {
+  const res = http.get('http://localhost:8080/');
+  check(res, { 'status is 200': (r) => r.status === 200 });
+  sleep(1);
+}
