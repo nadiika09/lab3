bcdf81138f52b8a7edfa588f532ad36abb0e9aed
nadiika09 <nadiika09@ukr.net>
2026-04-10 04:14:46 +0800

    Update stress-test.js

--- performance-tests/stress-test.js
+++ performance-tests/stress-test.js
@@ -1,1 +1,14 @@
+import http from 'k6/http';
+import { check } from 'k6';
 
+export const options = {
+  vus: 30,
+  duration: '30s',
+};
+
+export default function () {
+  const res = http.get('http://localhost:8080/');
+  check(res, {
+    'response time < 500ms': (r) => r.timings.duration < 500,
+  });
+}
