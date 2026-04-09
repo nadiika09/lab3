4242d7df4b3bef39d0a0907769a9c493c03a4411
nadiika09 <nadiika09@ukr.net>
2026-04-10 04:18:00 +0800

    Update load-test.js

--- performance-tests/load-test.js
+++ performance-tests/load-test.js
@@ -1,1 +1,15 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
 
+export const options = {
+  stages: [
+    { duration: '5s', target: 5 },
+    { duration: '10s', target: 5 },
+    { duration: '5s', target: 0 },
+  ],
+};
+
+export default function () {
+  http.get('http://localhost:8080/');
+  sleep(1);
+}
